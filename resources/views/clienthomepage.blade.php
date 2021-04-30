7eeb31474e075c3fa3d28e29353fda0b0b2284ac
MarkF791 <80972020+MarkF791@users.noreply.github.com>
2021-04-30 10:30:52 +0100

    updated client pages

--- resources/views/clienthomepage.blade.php
+++ resources/views/clienthomepage.blade.php
@@ -5,9 +5,10 @@
 <div class="container-fluid" style="background-color:black; padding-top:50px">
     <div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/314478784?autoplay=1&loop=1&title=0&byline=0&portrait=0" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
 </div>
-<div class="container text-center">
+<div class="container-fluid text-center">
     <h1>Welcome to the Bluespec Digital Media Website</h1>
     <hr>
+    <img src="https://media-exp1.licdn.com/dms/image/C4D03AQHZC8iBz8u8pw/profile-displayphoto-shrink_200_200/0/1562363664683?e=1625097600&v=beta&t=3U2LjbpXM90CK9pSi99TvT9_VgX527EKhpU0BpopTsQ" alt="Gary Mellor Portrait" sizes="50px" srcset="">
     <img src="https://bluespec.co.uk/wp-content/uploads/2020/11/BS-LogoAndText-2020_Final-No-DropShadow_3840_CroppedMore-1536x363.jpg" class="img-fluid" alt="Responsive image">
     <p style="font-size:8mm; text-align:left;">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam architecto tenetur facere sapiente est commodi eligendi non, alias libero sunt nam in, soluta minus culpa maxime totam incidunt! Nobis, repellendus.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam architecto tenetur facere sapiente est commodi eligendi non, alias libero sunt nam in, soluta minus culpa maxime totam incidunt! Nobis, repellendus.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam architecto tenetur facere sapiente est commodi eligendi non, alias libero sunt nam in, soluta minus culpa maxime totam incidunt! Nobis, repellendus.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam architecto tenetur facere sapiente est commodi eligendi non, alias libero sunt nam in, soluta minus culpa maxime totam incidunt! Nobis, repellendus.</p>
 </div>
--- resources/views/clientlogoAnimation.blade.php
+++ resources/views/clientlogoAnimation.blade.php
@@ -2,9 +2,9 @@
 
 @section('content')
 <div id='white_div' class="container-fluid" style="background-color:black; padding-top:50px">
-    <iframe width="100%" height="600" src="https://player.vimeo.com/video/458918328?autoplay=1&loop=1" width="100%" height="600" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
+    <div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/458918328?autoplay=1&loop=1&title=0&byline=0&portrait=0" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
 </div>
-    <div class="container text-center">
+    <div class="container-fluid text-center">
     <h1>Logo Animation</h1>
     <hr>
     <p style="font-size:8mm; text-align:left;">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam architecto tenetur facere sapiente est commodi eligendi non, alias libero sunt nam in, soluta minus culpa maxime totam incidunt! Nobis, repellendus.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam architecto tenetur facere sapiente est commodi eligendi non, alias libero sunt nam in, soluta minus culpa maxime totam incidunt! Nobis, repellendus.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam architecto tenetur facere sapiente est commodi eligendi non, alias libero sunt nam in, soluta minus culpa maxime totam incidunt! Nobis, repellendus.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam architecto tenetur facere sapiente est commodi eligendi non, alias libero sunt nam in, soluta minus culpa maxime totam incidunt! Nobis, repellendus.</p>   
