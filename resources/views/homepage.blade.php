e0994e9d5482694952b5007fb9b40644c92bffe6
MarkF791 <80972020+MarkF791@users.noreply.github.com>
2021-05-21 16:09:21 +0100

    Changes with styling corrected for cards

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -6,10 +6,19 @@
     <div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/314478784?autoplay=1&loop=1&title=0&byline=0&portrait=0" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
 </div>
 <div class="container text-center">
-    <h1>Welcome to the Bluespec Digital Media Website</h1>
-    <hr>
     <img src="https://bluespec.co.uk/wp-content/uploads/2020/11/BS-LogoAndText-2020_Final-No-DropShadow_3840_CroppedMore-1536x363.jpg" class="img-fluid" alt="Responsive image">
-    <p style="font-size:8mm; text-align:left;">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam architecto tenetur facere sapiente est commodi eligendi non, alias libero sunt nam in, soluta minus culpa maxime totam incidunt! Nobis, repellendus.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam architecto tenetur facere sapiente est commodi eligendi non, alias libero sunt nam in, soluta minus culpa maxime totam incidunt! Nobis, repellendus.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam architecto tenetur facere sapiente est commodi eligendi non, alias libero sunt nam in, soluta minus culpa maxime totam incidunt! Nobis, repellendus.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam architecto tenetur facere sapiente est commodi eligendi non, alias libero sunt nam in, soluta minus culpa maxime totam incidunt! Nobis, repellendus.</p>
+    <div class="container text-center">
+	<hr>
+    <h1>MOTION IS EYE CATCHING</h1>
+    <hr>
+    <p style="font-size:8mm; text-align:left;">Getting noticed online is getting more competitive. Adding animated motion to your branding, product video or website engages your potential customers much more.</p>
+
+    <p style="font-size:8mm; text-align:left;">Bluespec Digital Media has been creating animation and motion graphics for over 20 years. 2D and 3D animations, corporate presentations, animated graphics for product videos, explainer videos and logo animations are just some of the moving media we can create to enhance and compliment your next creative project.</p>
+
+    <p style="font-size:8mm; text-align:left;">So come inside, take a look at our previous work and let us know what you're looking for.</p>
+
+    <p style="font-size:8mm; text-align:left;">We're sure we can help you find it.</p>
+</div>
 </div>
 
 
